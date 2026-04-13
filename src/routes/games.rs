fadbb59b1ff4784c6518e45911433505e2c6d8bf
Capiyo <engineercapiyo@gmail.com>
2026-04-13 22:28:05 +0300

    update

--- src/routes/games.rs
+++ src/routes/games.rs
@@ -1,16 +1,12 @@
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::get, Router};
 
-use crate::state::AppState;
 use crate::handlers::games;
+use crate::state::AppState;
 
 pub fn routes() -> Router<AppState> {
     Router::new()
-        .route("/", get(games::get_games))
+        .route("/", get(games::get_upcoming_games))
         .route("/stats", get(games::get_game_stats))
         .route("/recent", get(games::get_recent_games))
         .route("/:id", get(games::get_game_by_id))
-
 }
